a804441ba89162b6f78cef9d9c209000f9b2b072
ITh0rn <angelo.dalfonso@student.univaq.it>
2019-07-09 16:27:20 +0200

    update

--- Ionic - DailyPills/myApp/src/app/tab2/tab2.page.ts
+++ Ionic - DailyPills/myApp/src/app/tab2/tab2.page.ts
@@ -8,18 +8,20 @@ import { HTTP } from '@ionic-native/http/ngx';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  item: any;
+  item: [];
 
   constructor(private http: HttpClient, private nativeHTTP: HTTP) {
-    this.searchData();
-  }
-
-  searchData() {
     // @ts-ignore
-    const headers = new HttpHeaders();
-    headers.append('Content-Type', 'application/json' );
-    headers.append('Access-Control-Allow-Headers', 'application/json' );
-    headers.append('responseType', '\'text\' as \'json\'');
-    this.http.get('https://www.agenziafarmaco.gov.it/services/search/select?fl=sm_field_codice_farmaco,sm_field_descrizione_farmaco,sm_field_descrizione_ditta&q=bundle:confezione_farmaco+sm_field_descrizione_farmaco:tachi*&df=sm_field_descrizione_farmaco&wt=jsonp&rows=150000', { headers }).subscribe(res => { console.log(res); this.item = res; });
+    this.item = [{ nome: 'tachipriina'}, {nome: 'lexotan'}];
+
+    /*searchData() {
+      // @ts-ignore
+      const headers = new HttpHeaders();
+      headers.append('Content-Type', 'application/json' );
+      headers.append('Access-Control-Allow-Headers', 'application/json' );
+      headers.append('responseType', '\'text\' as \'json\'');
+      this.http.get('https://www.agenziafarmaco.gov.it/services/search/select?fl=sm_field_codice_farmaco,sm_field_descrizione_farmaco,sm_field_descrizione_ditta&q=bundle:confezione_farmaco+sm_field_descrizione_farmaco:tachi*&df=sm_field_descrizione_farmaco&wt=jsonp&rows=150000', { headers }).subscribe(res => { console.log(res); this.item = res; });
+    }*/
+
   }
 }

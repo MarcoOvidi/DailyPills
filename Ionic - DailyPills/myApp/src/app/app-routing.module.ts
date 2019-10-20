48d07fdb5f42777d8155289a0b736e82292badf1
Marco Ovidi <marco.ovidi.96@gmail.com>
2019-10-20 19:11:27 +0200

    impostazioni

--- Ionic - DailyPills/myApp/src/app/app-routing.module.ts
+++ Ionic - DailyPills/myApp/src/app/app-routing.module.ts
@@ -36,7 +36,8 @@ const routes: Routes = [
     path: 'addfarmaco', loadChildren: './pages/addfarmaco/addfarmaco.module#AddfarmacoPageModule'
   },
   { path: 'lista-piani', loadChildren: './pages/lista-piani/lista-piani.module#ListaPianiPageModule' },
-  { path: 'nuovo-piano', loadChildren: './pages/nuovo-piano/nuovo-piano.module#NuovoPianoPageModule' }
+  { path: 'nuovo-piano', loadChildren: './pages/nuovo-piano/nuovo-piano.module#NuovoPianoPageModule' },
+  { path: 'impostazioni', loadChildren: './pages/impostazioni/impostazioni.module#ImpostazioniPageModule' }
 ];
 @NgModule({
   imports: [

6a739ff15a1453beac91c300aed3d7e2dbcf24bb
ITh0rn <angelo.dalfonso@student.univaq.it>
2019-10-29 17:15:29 +0100

    new Tab

--- Ionic - DailyPills/myApp/src/app/pages/dettaglio-piano/dettaglio-piano.module.ts
+++ Ionic - DailyPills/myApp/src/app/pages/dettaglio-piano/dettaglio-piano.module.ts
@@ -6,12 +6,28 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { DettaglioPianoPage } from './dettaglio-piano.page';
-import {TranslateModule} from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [
   {
     path: '',
-    component: DettaglioPianoPage
+    component: DettaglioPianoPage,
+    children: [
+      {
+        path: 'dettaglio',
+        children: [
+          {
+            path: '',
+            loadChildren: '../dettaglio-piano-sub-tab-left/dettaglio-piano-sub-tab-left.module#DettaglioPianoSubTabLeftPageModule'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/piani/dettaglio-piano/dettaglio',
+        pathMatch: 'full'
+      }
+  ]
   }
 ];
 
--- Ionic - DailyPills/myApp/src/app/pages/lista-piani/lista-piani.module.ts
+++ Ionic - DailyPills/myApp/src/app/pages/lista-piani/lista-piani.module.ts
@@ -11,7 +11,7 @@ import {TranslateModule} from '@ngx-translate/core';
 const routes: Routes = [
   {
     path: '',
-    component: ListaPianiPage
+    component: ListaPianiPage,
   }
 ];
 
